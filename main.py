42f5f643f80e0b1cda2a0567eab51cd824cbfb2a
dayaman <s16t279@stu.kagawa-u.ac.jp>
2019-02-18 23:48:32 +0900

    ファイル削除にスリープ

--- main.py
+++ main.py
@@ -361,6 +361,7 @@ async def on_message(message):
         # 再生処理
         voice_mess = './cache/{}/{}'.format(str_guild_id, rawfile) # rawファイルのディレクトリ
         voice[guild_id].play(discord.FFmpegPCMAudio(voice_mess, before_options='-f s16be -ar 16k -ac 1')) # エンコードして音声チャンネルで再生
+        await asyncio.sleep(0.5)
         os.remove(voice_mess) #rawファイルの削除
 
 def add_guild_db(guild):
